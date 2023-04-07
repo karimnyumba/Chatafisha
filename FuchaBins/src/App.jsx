04214097977775f3b6f60c0def6a6564863ebbcb
Kareem643 <nyumba29@gmail.com>
2023-04-08 01:33:03 +0300

    navbar creation

--- FuchaBins/src/App.jsx
+++ FuchaBins/src/App.jsx
@@ -1,8 +1,10 @@
+import React from "react"
+import Navbar from "./components/nav"
 function App() {
   
   return (
     <div className="w-screen h-screen bg-red-300">
-
+    <Navbar />
 
     </div>
     
