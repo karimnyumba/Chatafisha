8c4d52a10d67df978da2a1ce7e08bcfb2bbab272
karimnyumba <nyumba29@gmail.com>
2023-04-25 22:54:28 +0300

    register api

--- FuchaBins/src/pages/RegisterForm.jsx
+++ FuchaBins/src/pages/RegisterForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from 'axios'
 
 const RegisterForm = () => {
   const [name, setName] = useState("");
@@ -7,6 +8,7 @@ const RegisterForm = () => {
   // const [date, setDate] = useState(getCurrentDate()); // initialize with current date
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
   const handleNameChange = (e) => setName(e.target.value);
   const handleContactChange = (e) => {
@@ -19,34 +21,40 @@ const RegisterForm = () => {
   // const handleDateChange = (e) => setDate(e.target.value);
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleRoleChange = (e) => setRole(e.target.value);
+  const handleSubmit = (e)=>{
+    
+    e.preventDefault()
+    
 
-  // function getCurrentDate() {
-  //   const today = new Date();
-  //   const month = today.getMonth() + 1; // add 1 since getMonth returns 0-11
-  //   const day = today.getDate();
-  //   const year = today.getFullYear();
-  //   return `${year}-${month < 10 ? "0" + month : month}-${day < 10 ? "0" + day : day}`;
-  // }
-
-  async function signup(){
-    let item = {name, email, contact, location}
-    console.warn(item)
-    let result = await fetch("https://localhost:8000/api/register",{
-      method:'POST',
-      body:JSON.stringify(item),
-      header:{ 
-        "Content-Type": 'application/json',
-        "Accept": 'application/json'
+    const postData = async function (){
+      console.log('dadfad')
+      try{
+        const {data}= await axios.post(
+          'http://139.162.249.220:9292/api/user/registration_data', {
+            'email':email,
+            'u_name':name,
+            'location':location,
+            'phone':contact,
+          'password':password,
+          'role':role,
+          'reg_no': '2021'
+          }
+        )
+        console.log(data)
+        // window.location.pathname = '/pages/LoginForm'
+      }catch(err){
+        console.log(err)
       }
-    })
-    result = await result.json()
-    console.warn("result", result)
-}
+      
+    }
+    postData()
+  }
   return (
     <div className="bg-baki bg-cover bg-center min-h-screen flex flex-col items-center justify-center">
       <div className="bg-green-500 rounded-lg px-8 py-6">
         <h2 className="text-2xl font-bold mb-4">Register Information</h2>
-        <form className="flex flex-col items-center justify-center">
+        <form className="flex flex-col items-center justify-center" onSubmit={(e)=>handleSubmit(e)}>
           <input
             type="text"
             placeholder="Name of Picker / Organization"
@@ -96,7 +104,18 @@ const RegisterForm = () => {
             value={password}
             onChange={handlePasswordChange}
           />
-          <button onClick={signup} className="bg-black text-green-500 rounded-lg px-4 py-2">
+          <select
+              className="bg-gray-100 rounded-lg px-4 py-2 mt-3 w-full mb-4"
+              value={role}
+              onChange={handleRoleChange}
+            >
+              <option value="">Select Role</option>
+              <option value="Picker">Picker</option>
+              <option value="Organization">Organization</option>
+              <option value="Explorer">Explorer</option>
+            </select>
+          
+          <button type='submit'className="bg-black text-green-500 rounded-lg px-4 py-2">
             Register
           </button>
         </form>
